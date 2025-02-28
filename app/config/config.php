f85fa182b90f16fe849cb567dff510d2cebfda82
Victoria Alysha F. S. <126405181+vafs92@users.noreply.github.com>
2025-02-28 19:28:47 +0700

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost:8081/Tugas_mvc/public');
-
-//DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'platform_proin');
